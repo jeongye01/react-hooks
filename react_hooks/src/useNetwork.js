8b0b8518f5ae07c5c84e5aa2638594ab77025916
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2021-09-15 05:46:47 +0900

    useNetwork

---
+++ react_hooks/src/useNetwork.js
@@ -0,0 +1,20 @@
+import { useEffect,useState } from 'react';
+export const useNetwork=onChange=>{
+  const [status,setStatus]=useState(navigator.online);
+  const handleChange=()=>{
+    if(typeof onChange==="function"){
+      onChange(navigator.online);
+   }
+   setStatus(navigator.online);
+
+  };
+  useEffect(()=>{
+     window.addEventListener("online",handleChange);
+     window.addEventListener("offline",handleChange);
+     ()=>{
+      window.removeEventListener("online",handleChange);
+      window.removeEventListener("offline",handleChange);
+     };
+  },[]);
+  return status;
+}
